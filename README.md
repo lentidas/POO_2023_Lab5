3f107ebf32d78e9f87e4a4434bdcf6c9e381396e
Gonçalo Heleno <goncalo.carvalheiroheleno@heig-vd.ch>
2023-10-17 16:45:29 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# POO_2023_Lab5
+
+## Groupe L04GrI
+
+- Gonçalo Carvalheiro Heleno
+- Sven Ferreira Silva
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Gonçalo Carvalheiro Heleno
+ * @author Sven Ferreira Silva
+ */
+
+public class Main {
+
+  public static void main(String... args) {
+
+  }
+}
